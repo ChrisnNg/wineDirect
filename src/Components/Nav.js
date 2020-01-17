46eb1df91c9b4118796dca28f3a6ef7f0d56e62e
ChrisnNg <cristopherng@hotmail.com>
2020-01-17 15:44:50 -0800

    animate shopping cart bounce

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,18 +1,29 @@
 import React from "react";
-import Navbar from "react-bootstrap/Navbar";
+import "./Nav.css";
+import { Nav, Navbar } from "react-bootstrap";
 
 export default function(props) {
   return (
     <Navbar bg="dark">
       <Navbar.Brand href="#home">
         <img
-          src={require("../logo.svg")}
+          src={require("./wineglass.png")}
           width="30"
           height="30"
           className="d-inline-block align-top"
           alt="React Bootstrap logo"
         />
+        <b>
+          {" "}
+          <i>WineDirect</i>
+        </b>
       </Navbar.Brand>
+      <Nav className="ml-auto">
+        <i
+          className="fas fa-shopping-cart"
+          onClick={x => console.log("cart clicked")}
+        />
+      </Nav>
     </Navbar>
   );
 }
