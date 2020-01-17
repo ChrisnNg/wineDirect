739369fb77af7beb4b6ab71369c79b257f286763
ChrisnNg <cristopherng@hotmail.com>
2020-01-17 15:01:45 -0800

    intialize navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
+import Nav from "./Components/Nav.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Nav />
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
---
+++ src/Components/Nav.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+
+export default function(props) {
+  return (
+    <Navbar bg="dark">
+      <Navbar.Brand href="#home">
+        <img
+          src="../logo.svg"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+          alt="React Bootstrap logo"
+        />
+      </Navbar.Brand>
+    </Navbar>
+  );
+}
